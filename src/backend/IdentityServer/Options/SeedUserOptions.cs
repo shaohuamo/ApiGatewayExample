89bc9bcc9cede7fcbdf798644f7a0c7d26915df5
Lambda_zb <zhongbo19940130@gmail.com-local>
2026-06-11 23:16:18 +0800

    feat: enhance IdentityServer configuration with database options and seed user settings

--- src/backend/IdentityServer/Options/SeedUserOptions.cs
+++ src/backend/IdentityServer/Options/SeedUserOptions.cs
@@ -5,7 +5,7 @@ public sealed class SeedUserOptions
     public const string SectionName = "SeedUser";
 
     public bool Enabled { get; set; }
-    public string UserName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public string UserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
 }
